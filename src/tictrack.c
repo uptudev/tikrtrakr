e3baaa9de71da710a79e1cf6362ae90b8fd336b4
uptu <uptu@uptu.dev>
2024-06-16 21:16:49 -0600

    Add client application to parse the JSON Not working yet, but succesfulyl subscribes to pipe files. Just needs a parser and a print call and it's done!

--- src/tictrack.c
+++ src/tictrack.c
@@ -1,6 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
+#include "daemon.h"
 
-int main() {
+int main(int argc, char **argv) {
+
+    /* Parse input */
+    if (argc != 2) {
+        fprintf(stderr, "%s Usage: %s <symbol pair>\n%s Example:\n\t%s XRPUSDT\n", err(), argv[0], info(), argv[0]);
+        return 1;
+    }
+    char filename[32];
+    sprintf(filename, "/tmp/tictrackd.%s.json", argv[1]);
+
+    FILE *fd = fopen(filename, "r");
+    if (fd == NULL) {
+        fprintf(stderr, "%s File /tmp/tictrackd.%s doesn't exist; please start the daemon with `tictrack %s`\n", err(), argv[1], argv[1]);
+        return 1;
+    }
+    fprintf(stderr, "%s File /tmp/tictrackd.%s exists\n", info(), argv[1]); 
     return 0;
 }
